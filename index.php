a5e7b6a4a91df5dcb7ec3807eb14e153abc9933c
Ibrahim <ibpasha1@umbc.edu>
2017-12-08 14:33:40 -0500

    updates

--- index.php
+++ index.php
@@ -1,117 +1,17 @@
-<!DOCTYPE html>
-  <html>
-    <head>
-    <title>Groove Auto Push</title>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-      <link rel="stylesheet" type="text/css" href="style.css">
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-    <body style="background-color:#34495e;">
-<body>
-<nav>
-    <div class="nav-wrapper">
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="">Groove Auto Push</a></li>
-      </ul>
-    </div>
-  </nav>
-
-  <form action="" method="POST">
-  <ul class="collapsible" data-collapsible="accordion">
-  <li>
-    <div class="collapsible-header">
-      <i class="material-icons">lock</i>
-      Big Commerce Info:
-      <span class="badge"></span></div>
-    <div class="collapsible-body"><p>
-    <div class="row">
-        <div class="input-field col s6">
-            <input value="" id="client_id" type="text" class="validate" name="client_id" required>
-                <label class="active" for="client_id">client id</label>
-                 </div>
-            </div>
-
-            <div class="row">
-        <div class="input-field col s6">
-            <input value="" id="api_key" type="text" class="validate"   name="api_key" required>
-                <label class="active" for="api_key">api key</label>
-                 </div>
-            </div>
-            
-            <div class="row">
-        <div class="input-field col s6">
-            <input value="" id="store_url" type="text" class="validate" name="store_url" required>
-                <label class="active" for="store_url">store url</label>
-                 </div>
-            </div>
-
-       
-                    <p class="center-align"></p>
-                    <p class="center-align">
-            <button class="button button1" type="submit" name="submit">create connection</button>
-         </p>
-</form>
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-      <script>
- 
-            $(document).ready(function() {
-            $('select').material_select();
-
-            $('.timepicker').pickatime({
-              default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-              fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-              twelvehour: false, // Use AM/PM or 24-hour format
-              donetext: 'OK', // text for done-button
-              cleartext: 'Clear', // text for clear-button
-              canceltext: 'Cancel', // Text for cancel-button
-              autoclose: false, // automatic close timepicker
-              ampmclickable: true, // make AM PM clickable
-              aftershow: function(){} //Function for after opening timepicker
-                   });
-     
-                 });
-        
-    </script>
-    </body>
-  </html>
 <?php
+ini_set('max_execution_time', 300);
 //Written by Ibrahim Pasha - 11/22/2017 
-//---------------------------------------------GET CREDS FROM USER---------------------------------------
+//---------------------------------------------GET CREDS FROM USER---------NO LONGER USED
 $store_id  = isset($_POST['store_id'])  ? $_POST['store_id']  : '';
 $store_url = isset($_POST['store_url']) ? $_POST['store_url'] : '';
 $client_id = isset($_POST['client_id']) ? $_POST['client_id'] : '';
 $api_key   = isset($_POST['api_key'])   ? $_POST['api_key']   : '';
 
-
-$v1 = $store_id;
-$v2 = $store_url;
-$v3 = $client_id;
-$v4 = $api_key;
-$cred = array($v1, $v2, $v3, $v4);
-$data = var_export($cred, true);
-$var1 = "<?php\n\n\$v1 = $data;\n\n?>";
-file_put_contents('cred.php', $var1);
+$store_url = 'https://api.bigcommerce.com/stores/ucycv5vmkf/v3/';
+$client_id = '3el5giojff6f0d9gn4fnnp1lhi03sfr';
+$api_key   = '2lxhch78sg9orr7ke3a94dj6tylsmt8';
 
 
-include 'cred.php';
-
-if ($store_id == '' && $store_id == '' && $client_id == '' && $api_key == '' ) 
-{
-
-}
-// I need to add globals to this 
-$v1[0] = $GLOBALS['store_id'];
-$v1[1] = $GLOBALS['store_url'];
-$v1[2] = $GLOBALS['client_id'];
-$v1[3] = $GLOBALS['api_key'];
-//---------------------------------------------GET PRODUCT INFO FROM API-----------------------------------
 function groove_trail_slash($string) 
 {
 	return rtrim($string, '/');
@@ -154,9 +54,12 @@ function groove_getOrder(&$ch, $orderid)
 	return runcurl($ch, $api_url);
 }
 
-function groove_getProducts(&$ch, $name) 
+
+function groove_getProducts(&$ch, $x) 
 {
-	$api_url = groove_trail_slash(groove_getStoreUrl()) . '/catalog/products/?include=images';// LETS TAKE A LOOK
+	//$x = $GLOBALS['x'];
+	$api_url = groove_trail_slash(groove_getStoreUrl()) . '/catalog/products/?include=images&page='.$x.'&limit=250';// LETS TAKE A LOOK
+	//$api_url = groove_trail_slash(groove_getStoreUrl()) . '/catalog/products?page='.$page_num.'/?include=images';// LETS TAKE A LOOK
 	$ch = curl_init();
 	return runcurl($ch, $api_url);
 }
@@ -215,78 +118,101 @@ function runcurl(&$ch, $api_url = '') {
 	$response = curl_exec($ch);
 	return $response;
 }
-session_start();
-sleep(2);
-$name      = '';
-$productid  = '';
 
+$name       = '';
+$productid  = '';
+//$value = 1;
+$page_value = '';
+$y = 0;
 // Brand call
 groove_getBrand($ch, $productid);
 $brand_name = groove_getBrand($ch, $productid);
 $data_set_6 = json_decode($brand_name,JSON_PRETTY_PRINT);
-$fp = fopen('brand.json', 'w');
-fwrite($fp, json_encode($data_set_6, JSON_PRETTY_PRINT));
-fclose($fp);
-//
+//$fp = fopen('brand.json', 'w');
+//fwrite($fp, json_encode($data_set_6, JSON_PRETTY_PRINT));
+//fclose($fp);
 
-//Product information & image information
-groove_getProducts($ch, $name);
-$json5 = groove_getProducts($ch, $name); // general product information
+$page = 6;
 
-$data_set_5 = json_decode($json5,JSON_PRETTY_PRINT);
-$fp = fopen('test.json', 'w');
-fwrite($fp, json_encode($data_set_5, JSON_PRETTY_PRINT));
-fclose($fp);
-//
+$product_list_1 = groove_getProducts($ch, $y); // the array is built
+for ($x = 1; $x <= $page; $x++) 
+{
+	$product_list_2 = groove_getProducts($ch, $x); // the array is built
+	$v[] = json_decode($product_list_2, true);
+	$json_merge = json_encode($v);
+} 
+$v[] = json_decode($product_list_1, true);
 
-
-	$array = json_decode($json5, JSON_PRETTY_PRINT);
-    $fp = fopen('data.csv',  'w');
-		$header = false;
+//$total_list = array_merge($stack_1, $product_list);
+$total_list = $json_merge;
+//----------------------------------------------------------------------------
+$data_set_5 = json_decode($total_list,JSON_PRETTY_PRINT);
+//4504
+//----------------------------------------------------------------------------
+//$fp = fopen('test.json', 'w');
+//fwrite($fp, json_encode($data_set_5, JSON_PRETTY_PRINT));
+//fclose($fp);
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+$array = json_decode($total_list, JSON_PRETTY_PRINT);
+$fp = fopen('data.csv',  'w');
+	$header = false;
 		
-
-		foreach ($array["data"] as $row)
+	foreach ($array as $row) 
+	{
+		foreach ($row["data"] as $row2) 
 		{
-
-			foreach ($row["images"] as $second_row) {
-				$image_link                = $second_row['url_standard'];
+			
+			foreach ($row2["images"] as $second_row) 
+			{
+				$image_link    = $second_row['url_standard'];
 			}
-				$id                        = $row['id'];
-				$title           	       = $row['name'];
-				$meta_description          = $row['meta_description'];
-				$availability              = $row['availability'];
-				$condition                 = $row['condition'];
-				$price           	       = $row['price'];
-				$link                      = $row['custom_url']['url'];
-				//$image_link                = $row['images']['url_standard'];
-				$brand                     = $row['brand_id'];
-				$layout_file               = $row['layout_file'];
-				$age_group                 = $row['search_keywords'];
-				$color                     = $row['search_keywords'];
-				$gender                    = $row['search_keywords'];
-				$item_group_id             = $row['search_keywords'];
-				$google_product_category   = $row['search_keywords'];
-				$material                  = $row['search_keywords'];
-				$pattern                   = $row['search_keywords'];
-				$product_type              = $row['search_keywords'];
-				$sale_price                = $row['sale_price'];
-				$sale_price_effective_date = $row['date_created'];
-				$shipping                  = $row['is_free_shipping'];
-				$shipping_weight 		   = $row['weight'];
-				$custom_label_0 		   = $row['search_keywords'];
-				$custom_label_1  		   = $row['search_keywords'];
-				$custom_label_2 		   = $row['search_keywords'];
-				$custom_label_3  		   = $row['search_keywords'];
-				$custom_label_4  		   = $row['search_keywords'];
-				$page_count                = $row['meta']['pagination']['total_pages'];
 
-				// 
+					foreach ($array[0]["meta"] as $third_row) 
+					{
+						$page_value          = $third_row['total_pages'];
+						//$page_value          = $GLOBALS['page_value'];
+						$total_products      = $third_row['total'];
+						$per_page			 = $third_row['per_page'];
+					}
+
+
+				$id                        = $row2['id'];
+				$title           	       = $row2['name'];
+				$description               = $row2['description'];
+				$availability              = $row2['availability'];
+				$condition                 = $row2['condition'];
+				$price           	       = $row2['price'];
+				$link                      = $row2['custom_url']['url'];
+				//$image_link                = $row['images']['url_standard'];
+				$brand                     = $row2['brand_id'];
+				$layout_file               = $row2['layout_file'];
+				$age_group                 = $row2['search_keywords'];
+				$color                     = $row2['search_keywords'];
+				$gender                    = $row2['search_keywords'];
+				$item_group_id             = $row2['search_keywords'];
+				$google_product_category   = $row2['search_keywords'];
+				$material                  = $row2['search_keywords'];
+				$pattern                   = $row2['search_keywords'];
+				$product_type              = $row2['search_keywords'];
+				$sale_price                = $row2['sale_price'];
+				$sale_price_effective_date = $row2['date_created'];
+				$shipping                  = $row2['is_free_shipping'];
+				$shipping_weight 		   = $row2['weight'];
+				$custom_label_0 		   = $row2['search_keywords'];
+				$custom_label_1  		   = $row2['search_keywords'];
+				$custom_label_2 		   = $row2['search_keywords'];
+				$custom_label_3  		   = $row2['search_keywords'];
+				$custom_label_4  		   = $row2['search_keywords'];
+
+				//-----------filters out the html in description-----------------------//
+
+				$flitered_des = strip_tags($description);
 				if($availability == 'disabled') {
 					$note = 'out of stock';
 				} else if ($availability == 'available') {
 					$note = 'in stock';
 				}
-
 
 				if($brand == '0') {
 					$brand_name = 'Unknown';
@@ -303,19 +229,10 @@ fclose($fp);
 				} else if ($brand == '6') {
 					$brand_name = 'Briess';
 				} 
-
-				//this function will read how many pages the store has and lopp through the page numbers to
-				//list all possible products
-
-
-				//this function will strip the description tag of all its html tags and output a fresh description
-
-
 				$web_url = 'eckraus.com';
 				$mod_link = $web_url . $link;
-
 	
-				$contents = array("id"=>"$id","title"=>$title,"meta_description"=>$meta_description
+				$contents = array("id"=>"$id","title"=>$title,"description"=>$flitered_des
 				,"availability"=>$note,"condition"=>$condition,"price"=>$price
 				,"link"=>$mod_link,"image_link"=>$image_link,"brand"=>$brand,"brand"=>$brand_name
 				,"additional_image_link"=>$layout_file, "age_group"=>$age_group, "color"=>$color
@@ -341,9 +258,16 @@ fclose($fp);
 				fputcsv($fp, $header);
 				$header = array_flip($header);
 			}
-			fputcsv($fp, array_merge($header, $contents));
-		}
+			fputcsv($fp, array_merge($header, $contents));	
+	}
+}
+		echo "Total Pages    = ------------------:" . $page_value;
+		echo "</br>";
+		echo "Total Products = ------------------:" . $total_products;
+		echo "</br>";
+		echo "Per Page Value = ------------------:" . $per_page;
+		echo "</br>";
+		//echo "Test ID        = ------------------:" . $new_id;
 		fclose($fp);
 		return;
-	
 ?>
